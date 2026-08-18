6f806a0676dcfd97b73a19d9f72314ba6812f07c
Thiago Gama <tgsmoreira@gmail.com>
2026-08-18 14:44:28 -0300

    terminei a conexao com o banco: retry de 10 tentativas e config do pool

--- estoque/internal/database/database.go
+++ estoque/internal/database/database.go
@@ -1,7 +1,45 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"log"
+	"time"
 
-func ConectarBanco(URLBanco string) (*gorm.DB, error) {
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
+func tentarConectar(URLBanco string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(URLBanco), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	err = sqlDB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(25)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	return db, nil
+}
+func Conectar(URLBanco string) (*gorm.DB, error) {
+	var ultimoErro error
+	for i := 0; i <= 9; i++ {
+		db, err := tentarConectar(URLBanco)
+		if err == nil {
+			return db, nil
+		}
+		ultimoErro = err
+		log.Printf("Banco indisponível (tentativa %d/10): %v", i, err)
+		if i < 10 {
+			time.Sleep(2 * time.Second)
+		}
+	}
+	return nil, ultimoErro
 }
